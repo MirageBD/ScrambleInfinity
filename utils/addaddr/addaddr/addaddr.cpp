1454917a45aa8195c16173ba8e4356cc2fa43f54
MirageBD <lars.verhoeff@ntlworld.com>
2021-08-04 20:39:46 +0100

    Adding utils source.

---
+++ utils/addaddr/addaddr/addaddr.cpp
@@ -0,0 +1,38 @@
+// project->properties->Configuration Properties->c/C++->Preprocessor->Preprocessor Definitions-> add ;_CRT_SECURE_NO_DEPRECATE
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+	FILE *infile, *outfile;
+	unsigned char in;
+
+	if(argc < 4)
+	{
+		printf("\nUsage: addaddr [infile] [outfile] [startaddress]\n");
+		exit(1);
+	}
+
+	infile = fopen(argv[1], "rb");
+	outfile = fopen(argv[2], "wb");
+	int start = atoi(argv[3]);
+
+	in = start%256;
+	fwrite((const void *) &in, sizeof(unsigned char), 1, outfile);
+	in = start/256;
+	fwrite((const void *) &in, sizeof(unsigned char), 1, outfile);
+
+	while(fread(&in,sizeof(unsigned char),1,infile))
+	{
+		fwrite((const void *) &in, sizeof(unsigned char), 1, outfile);
+	}
+
+	fclose(outfile);
+	fclose(infile);
+
+	return 0;
+}
+
