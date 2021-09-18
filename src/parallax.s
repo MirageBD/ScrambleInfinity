59c7f2a53223022b0ba5fd4ae6e101af71714558
MirageBD <lars.verhoeff@ntlworld.com>
2021-09-18 20:13:56 +0100

    Rewriting to take up less space and the same amount of cycles in the worst case.

--- src/parallax.s
+++ src/parallax.s
@@ -1,39 +1,5 @@
 .segment "PARALLAX"
 
-.macro checkorplotstar arg0
-    ldx starsallowedtodraw+arg0
-    beq :+
-    .ident(.sprintf("ds%s", .string(arg0))):
-    sta bitmap1
-:
-.endmacro
-
-.macro checkstar arg0
-    .ident(.sprintf("cs%s", .string(arg0))):
-    lda bitmap1
-    cmp #%01010101
-    bne :+
-    stx starsallowedtodraw+arg0
-:
-.endmacro
-
-.macro setupstarplot arg0, arg1
-    sta arg0+2
-	adc #arg1
-.endmacro
-
-.macro setupstarcheckplot arg0, arg1, arg2
-    sta arg0+2
-    sta arg1+2
-	adc #arg2
-.endmacro
-
-.macro setstarpattern arg0, arg1
-    sta arg0+1
-    sta arg1+1
-.endmacro
-
-
 drawstars
 
 drawstarsoffsetlo
@@ -65,71 +31,77 @@ starsplothiadd256
     lda #%01010101                      ; clear previous stars
     jsr dostarplot
 
-    cpy #$07                            ; if it's 7 check if the bitmap to plot to is clear
-    bne setupstarplotptrs
-    jsr setupcheckplotptrs
-    jmp doactualstarplot
-
-setupstarplotptrs
     clc
-    lda starsoffset0or100
-    setupstarplot ds0, 2
-    setupstarplot ds1, 4
-    setupstarplot ds2, 3
-    setupstarplot ds3, 3
-    setupstarplot ds4, 3
-    setupstarplot ds5, 3
-
-    ldx vspoffset+1
+    lda starsoffset0or100               ; set up high byte of check/plot addresses
+    sta ds1+2
+    sta cs1+2
+	adc #$02
+    sta ds2+2
+    sta cs2+2
+	adc #$04
+    sta ds3+2
+    sta cs3+2
+	adc #$03
+    sta ds4+2
+    sta cs4+2
+	adc #$03
+    sta ds5+2
+    sta cs5+2
+	adc #$03
+    sta ds6+2
+    sta cs6+2
+    
+    ldx vspoffset+1                     ; set up low byte of check/plot addresses
     lda times8lowtable,x
-    sta ds0+1
     sta ds1+1
+    sta cs1+1
     sta ds2+1
+    sta cs2+1
     sta ds3+1
+    sta cs3+1
     sta ds4+1
+    sta cs4+1
     sta ds5+1
+    sta cs5+1
+    sta ds6+1
+    sta cs6+1
+
+    cpy #$07                            ; if it's 7 check if the bitmap to plot to is clear
+    beq setupcheckplotptrs
 
 doactualstarplot
     lda starsoffset,y
-    jsr dostarplot
-    rts
-
-; -----------------------------------------------------------------------------------------------
-
 
 dostarplot
-
-    checkorplotstar 0
-    checkorplotstar 1
-    checkorplotstar 2
-    checkorplotstar 3
-    checkorplotstar 4
-    checkorplotstar 5
-
+    ldx starsallowedtodraw+0
+    beq :+
+ds1 sta bitmap1							; bitmap 1 or 2 (+$0100)
+:
+    ldx starsallowedtodraw+1
+    beq :+
+ds2 sta bitmap1
+:
+    ldx starsallowedtodraw+2
+    beq :+
+ds3 sta bitmap1
+:
+    ldx starsallowedtodraw+3
+    beq :+
+ds4 sta bitmap1
+:
+    ldx starsallowedtodraw+4
+    beq :+
+ds5 sta bitmap1
+:
+    ldx starsallowedtodraw+5
+    beq :+
+ds6 sta bitmap1
+:
     rts
 
 ; -----------------------------------------------------------------------------------------------
 
 setupcheckplotptrs
-
-    clc
-    lda starsoffset0or100
-
-    setupstarcheckplot ds0, cs0, 2
-    setupstarcheckplot ds1, cs1, 4
-    setupstarcheckplot ds2, cs2, 3
-    setupstarcheckplot ds3, cs3, 3
-    setupstarcheckplot ds4, cs4, 3
-    setupstarcheckplot ds5, cs5, 3
-
-    ldx vspoffset+1
-    lda times8lowtable,x
-    setstarpattern ds0, cs0
-    setstarpattern ds1, cs1
-    setstarpattern ds2, cs2
-    setstarpattern ds3, cs3
-    setstarpattern ds4, cs4
-    setstarpattern ds5, cs5
 
     lda #$00
     sta starsallowedtodraw+0
@@ -140,14 +112,38 @@ setupcheckplotptrs
     sta starsallowedtodraw+5
 
     ldx #$01
-    checkstar 0
-    checkstar 1
-    checkstar 2
-    checkstar 3
-    checkstar 4
-    checkstar 5
 
-    rts
+cs1 lda bitmap1
+    cmp #%01010101
+    bne :+
+    stx starsallowedtodraw+0
+:
+cs2 lda bitmap1
+    cmp #%01010101
+    bne :+
+    stx starsallowedtodraw+1
+:
+cs3 lda bitmap1
+    cmp #%01010101
+    bne :+
+    stx starsallowedtodraw+2
+:
+cs4 lda bitmap1
+    cmp #%01010101
+    bne :+
+    stx starsallowedtodraw+3
+:
+cs5 lda bitmap1
+    cmp #%01010101
+    bne :+
+    stx starsallowedtodraw+4
+:
+cs6 lda bitmap1
+    cmp #%01010101
+    bne :+
+    stx starsallowedtodraw+5
+:
+    jmp doactualstarplot
 
 ; -----------------------------------------------------------------------------------------------
 
@@ -166,7 +162,5 @@ starsoffset
     .byte %01100101
     .byte %10010101
     .byte %10010101
-
-    ;.byte %01010101 ; empty
 
 ; -----------------------------------------------------------------------------------------------
